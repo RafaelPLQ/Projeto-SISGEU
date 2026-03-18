8f3c79397b07a9a221dcd9633b79cb351e2aaca0
Rafael Podestá do Líbano Queiroz <12703670648@MUZAMBINHO.EDU.BR>
2026-03-17 22:47:39 -0300

    Atualicao Main

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(max_length=255, unique=True)
+    senha = models.CharField(max_length=255)
+    curso = models.CharField(max_length=150, blank=True, null=True)
+    instituicao = models.CharField(max_length=200, blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
